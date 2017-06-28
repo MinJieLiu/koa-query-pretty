90a1d6cfed1ad2af84ee514d4827ab7ccbd19fd4
liumy <liumingyi@zhinanmao.com>
2017-06-28 17:39:37 +0800

    init

---
+++ test/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { describe, it } = require('mocha');
+const assert = require('assert');
+const Koa = require('koa');
+const request = require('supertest');
+
+describe('koa-query-pretty', () => {
+  it('base', (done) => {
+    assert(1);
+    done();
+  });
+});
